7b7a2f0452463db4b23642304eb2189820165b3a
MaeschDEV <malteesch1508@gmail.com>
2025-08-23 22:08:31 +0200

    Style Changes
    - Formatted all documents
    - Slightly changed css code to fit better on mobile

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function getRandomItem() {
-    const randomIndex = Math.floor(Math.random() * 30);
-    window.location.href = `result.html?index=${randomIndex}`;
+  const randomIndex = Math.floor(Math.random() * 30);
+  window.location.href = `result.html?index=${randomIndex}`;
 }
